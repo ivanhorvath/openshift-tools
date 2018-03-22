0e40672b28cf845f70ff4525ce2edf999524cd30
Ivan Horvath <ihorvath@redhat.com>
2018-03-22 10:51:04 -0400

    adding some logging to zabbix email script

--- docker/oso-zabbix-server/src/smtp_ses.py
+++ docker/oso-zabbix-server/src/smtp_ses.py
@@ -6,6 +6,20 @@ Email script for zabbix notifications
 import smtplib
 import sys
 import yaml
+import tempfile
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+logger = logging.getLogger()
+logFormatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+logFile = '/var/log/zabbix/email_send.log'
+
+logRFH = RotatingFileHandler(logFile, mode='a', maxBytes=2*1024*1024, backupCount=5, delay=0)
+logRFH.setFormatter(logFormatter)
+logRFH.setLevel(logging.INFO)
+logger.addHandler(logRFH)
+logger.setLevel(logging.INFO)
+
 
 def get_config():
     '''Fetch the zabbix config credentials
@@ -27,21 +41,52 @@ def email():
         print 'Please provide the necessary variables.'
         sys.exit(1)
 
-    to_addr = sys.argv[1]
-    subject = sys.argv[2]
-    body = sys.argv[3]
+    try:
+        to_addr = sys.argv[1]
+        subject = sys.argv[2]
+        body = sys.argv[3]
 
-    msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (mailfrom, ", ".join(to_addr.split(',')), subject))
+        msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (mailfrom, ", ".join(to_addr.split(',')), subject))
 
-    msg += body
+        msg += body
 
-    server = smtplib.SMTP_SSL(smtpserver, 465, smtpdomain)
-    # server.set_debuglevel(1)
-    server.login(username, password)
-    server.sendmail(mailfrom, to_addr, msg)
-    server.quit()
-
+        server = smtplib.SMTP_SSL(smtpserver, 465, smtpdomain)
+        server.set_debuglevel(1)
+        try:
+            server.login(username, password)
+            server.sendmail(mailfrom, to_addr, msg)
+        except Exception as ex:
+            logger.info('Error sending message: %s', ex)
+    finally:
+        server.quit()
 
 if __name__ == '__main__':
+    logger.info('Getting ready to send an email, with params: %s', str(sys.argv))
+    if len(sys.argv) < 4:
+        logger.error('Not enough parameters!')
+        sys.exit(1)
+    # Find an available file descriptor
+    t = tempfile.TemporaryFile(dir='/tmp')
+    available_fd = t.fileno()
+    t.close()
+
+    # now make a copy of stderr
+    os.dup2(2,available_fd)
+
+    # Now create a new tempfile and make Python's stderr go to that file
+    t = tempfile.TemporaryFile(dir='/tmp')
+    os.dup2(t.fileno(),2)
+
     email()
 
+    # Grab the stderr from the temp file
+    sys.stderr.flush()
+    t.flush()
+    t.seek(0)
+    stderr_output = t.read()
+    logger.info('Sending email:\n%s', stderr_output)
+    t.close()
+
+    # Put back stderr
+    os.dup2(available_fd,2)
+    os.close(available_fd)
